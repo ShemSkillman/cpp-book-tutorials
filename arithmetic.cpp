91c67daf974ffb97c4dfb3b1ccb1bf8b3ffcfbdc
Shem Skillman <shemskillman@outlook.com>
2020-07-17 14:26:25 +0100

    Arithmetic script

---
+++ arithmetic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 8, b= 4;
+	
+	cout << "A: " << a << " B: " << b << endl;
+	cout << "A + B: " << a + b << endl;
+	cout << "A - B: " << a - b << endl;
+	cout << "A * B: " << a * b << endl;
+	cout << "A / B: " << a / b << endl;
+	cout << "A % B: " << a % b << endl;
+	cout << "A++: " << a++ << endl;
+	cout << "A: " << a << endl;
+	cout << "++B: " << ++b << endl;
+	cout << "B: " << b << endl;
+	
+	return 0;
+}
