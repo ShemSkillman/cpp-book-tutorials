14d3e9ee2c58bed0d45abdb871919fe94c86de1f
Shem Skillman <shemskillman@outlook.com>
2020-07-20 14:52:44 +0100

    String script

---
+++ string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string text = "9";
+	string term("9 ");
+	string info = "Toys";
+	string color;
+	
+	char hue[4] = {'R', 'E', 'D', '\0'};
+	color = hue;
+	info = " Balloons";
+	
+	text += (term + color + info);
+	cout << endl << text << endl;
+	
+	return 0 ;
+}
